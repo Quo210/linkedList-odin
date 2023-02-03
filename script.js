1c6026e970afe60cfe56bd480a77ba0de18e3a88
Gabriel <gabrielrengifo211@gmail.com>
2023-02-03 17:22:27 -0400

    all methods were implemented

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
 class LinkedList {
     constructor(){
-        this.name = 'Linked List',
-        this.size = 0
-        this.headNode = null
+        this.name = 'Linked List';
+        this.size = 0;
+        this.headNode = null;
     }
 
-    append(value){
+    append(node){
         let selector;
-        let myNode = new Node(value);
+        let myNode = node;
+        let instanceCheck = node instanceof Node;
+        console.log(node,'is this an instance of class Node?',instanceCheck)
+        if(!instanceCheck){
+            myNode = new Node(node);
+        }
         let headNode = this.headNode;
         if(headNode == null){//Placing the selector 
             this.headNode = myNode;
-            selector = headNode;
             this.size++
         } else {
             selector = this.#findLast(headNode);
+            console.log('Last node fonund was',selector)
             selector.nextNode = myNode
             this.size++
         }
         
+    }
+
+    preprend(val){
+        let oldHead = this.headNode;
+        let newNode = new Node(val);
+        if(oldHead == null){
+            this.headNode = newNode;
+            this.size++
+        } else {
+            newNode.next = oldHead;
+            this.headNode = newNode;
+            this.size++
+        }
+    }
+
+    /*          -----------------------------            */
+
+    get listSize(){
+        return 'This list has a size of ' + this.size;
     }
 
     get head(){
@@ -29,27 +53,116 @@ class LinkedList {
         this.headNode = val;
     }
 
+    get tail(){
+        if(this.headNode == null){
+            return null
+        }
+        const lastNode = this.#findLast(this.headNode);
+        return lastNode
+    }
+
+    at(index){
+        const store = this.#mkNodeArray();
+        return (store[index])? store[index] : 'Not found'; 
+
+    };
+    pop(){//Using Arrays
+        const list = this.#mkNodeArray();
+        console.log('List used:',list)
+        if(list.length>=2){
+            list[list.length-2].next = null;
+            this.size--;
+        } else {
+            this.headNode = null;
+            this.size = 0
+        }
+        return this
+    };
+    popV2(){// Not using Arrays
+        let init = this.headNode;
+        if(!init || !init.nextNode){
+            this.headNode = null;
+            return false
+        }
+        let next = init.nextNode;
+        while(next.nextNode){
+            init = next;
+            next = next.nextNode;
+        }
+        init.next = null;
+        return this.#mkNodeArray();
+    };
+    contains(value){
+        let init = this.headNode;
+        let result = false;
+        while(init != null && result != value){
+            result = init.data;
+            init = init.nextNode;
+            console.log('cycling',result,init)
+        }
+        return (result == value)? true : false;
+    };
+    find(value){
+        let arr = this.#mkNodeArray();
+        let res = false;
+        for (let i = 0; res != true && i < arr.length; i++){
+            (arr[i].data == value)? res = true : false
+        } return res
+    };
+    toString(){
+        let init = this.headNode;
+        let storeString = '';
+        while (init !== null && init.data){
+            console.log(init)
+            storeString += `( ${init.data} } -> `;
+            init = init.nextNode;
+        }
+        return storeString += 'null'
+    }
+
+
+
     #findLast(node){
+        console.log('#findLast execution on',node)
         let lastNode;
-        if(node.next != null){
-            lastNode = this.#findLast(node.next)
+        if(node.nextNode != null){
+            lastNode = this.#findLast(node.nextNode)
         } else {
             lastNode = node;
         }
         return lastNode
-    }
-
+    };
+    #mkNodeArray(){
+        const head = this.headNode;
+        if(head == null){
+            console.error('This list is empty.')
+            return false
+        }
+        const store = [head];
+        let target = head;
+        while(target.nextNode != null){
+            target = target.nextNode;
+            store.push(target)
+        }
+        return store;
+    };
 }
 
 class Node {
 
     constructor(data, nextOne = null){
-        this.data = data, 
-        this.nextNode = nextOne
+        this.data = data; 
+        this.nextNode = nextOne;
+    };
+    set next(val){
+        this.nextNode = val
     }
-
 
 }
 
 const a = new LinkedList();
+a.append(2)
+a.preprend(1)
+a.append(3)
+
 console.log(a)
